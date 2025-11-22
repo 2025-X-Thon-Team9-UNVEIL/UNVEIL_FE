9ca14055080ef4633b0d90357bb70138884e84a4
hansol65 <whgksthf65@gmail.com>
2025-11-23 01:11:47 +0900

    ♻️ refactor error 주석처리

--- src/pages/SigninPage/SigninPage.tsx
+++ src/pages/SigninPage/SigninPage.tsx
@@ -10,7 +10,7 @@ export default function SigninPage() {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [, /*error*/ setError] = useState('');
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
